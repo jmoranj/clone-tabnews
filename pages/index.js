daa9182669ed714c146de0dbc5bd322b8c805874
jmoranj <morandim.joao@gmail.com>
2025-11-13 00:01:09 -0300

    Add initial project structure with Next.js setup, including package.json, .gitignore, and basic styling

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Dengue from "./dengue";
+
+function Home() {
+  return (<>
+    <div className="container">
+      <h1>zzzzzzzzzzzzzzzzzzz</h1>
+      <Dengue/>
+    </div>
+  </>
+  )
+}
+export default Home;
