c2c6d30c478a3add1785d8e5719dc0582885fb3b
jmoranj <morandim.joao@gmail.com>
2026-09-15 22:53:10 -0300

    add `database.js` and `.env` files.

---
+++ infra/database.js
@@ -0,0 +1,20 @@
+import { Client } from "pg";
+
+async function query(queryObject){
+  const client = new Client({
+    host: process.env.POSTGRES_HOST ?? "localhost",
+    port: process.env.POSTGRES_PORT ?? 5432,
+    user: process.env.POSTGRES_USER ?? "postgres",
+    database: process.env.POSTGRES_DB ?? "postgres",
+    password: process.env.POSTGRES_PASSWORD ?? "local_pass"
+  })
+  await client.connect()
+  const result = await client.query(queryObject)
+  await client.end()
+  return result
+}
+
+export default {
+  query: query
+}
+
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,8 @@
-function status(request, response) {
-  response.status(200).json({ Chave: "valor" })
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query('SELECT 1 + 1')
+  response.status(200).json({ Chave: {result} })
 }
 
 export default status
