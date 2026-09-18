7d2fed96eb88f2bb47e3bfecf8d6344e0c08b967
jmoranj <morandim.joao@gmail.com>
2026-09-17 22:06:08 -0300

    add new `jsconfig.json` file with `@` paths resolution

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,7 @@
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query('SELECT 1 + 1')
+  const result = await database.query('SELECT 1 + 1 as sum')
   response.status(200).json({ Chave: {result} })
 }
 
